9e56be0a8cf44a7dd32cc500d2a85dca0ad320be
Kenny Huynh <hnkworkmail.511@gmail.com>
2026-07-21 12:09:21 +1000

    Based of the Calculator

---
+++ Commission_Calculator/Commission_Calculator/Commission_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Commission_CalculatorApp.swift
+//  Commission_Calculator
+//
+//  Created by Kenny on 21/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct Commission_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
